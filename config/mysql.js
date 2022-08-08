83313dd7937b9a107f9cfc162ef3550213cf7217
Achmad Safiul Ubab <safiulred@gmail.com>
2022-08-09 02:35:19 +0700

    update config db

--- config/mysql.js
+++ config/mysql.js
@@ -15,11 +15,11 @@ module.exports = {
         }
     },
     production : {
-        HOST: process.env.MYSQL_HOST,
-        PORT: process.env.MYSQL_PORT,
-        USER: process.env.MYSQL_USER,
-        PASSWORD: process.env.MYSQL_PASSWORD,
-        DB: process.env.MYSQL_DBNAME,
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DBNAME,
         dialect: "mysql",
         logging: false,
         pool: {
